489371b0fd76000900ef213cb43dcabcfcf708ec
wind-so-cool <xjq145882145882@163.com>
2020-07-28 12:35:22 +0800

    乐优商城后台管理

---
+++ src/pages/item/Category.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card>
+      <v-flex xs12 sm10>
+        <v-tree url="/item/category/list"
+                :isEdit="isEdit"
+                @handleAdd="handleAdd"
+                @handleEdit="handleEdit"
+                @handleDelete="handleDelete"
+                @handleClick="handleClick"
+        />
+      </v-flex>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "category",
+    data() {
+      return {
+        isEdit:true
+      }
+    },
+    methods: {
+      handleAdd(node) {
+        console.log("add .... ");
+        console.log(node);
+        this.$http.post("/item/category",node);
+      },
+      handleEdit(id, name) {
+        this.$http.put("/item/category",{
+          id:id,
+          name:name
+        })
+
+      },
+      handleDelete(id) {
+         this.$http.delete("/item/category",{
+           params:{
+             id:id
+           }
+
+        });
+      },
+      handleClick(node) {
+        console.log(node)
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
